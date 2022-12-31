856671fe76304bc0ec700d369cfbcf708d8d3275
Neel Smith <dnsmith.neel@gmail.com>
2022-12-31 10:31:44 -0500

    Add headers

--- pluto/dev.jl
+++ pluto/dev.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.18
+# v0.19.19
 
 using Markdown
 using InteractiveUtils
@@ -26,6 +26,12 @@ end
 # ╔═╡ 2a2d0160-8794-11ed-23a7-0d6ff795640e
 md"""# Dev. notebook"""
 
+# ╔═╡ 99a2e366-4e46-401b-bae9-241e98425e97
+md"""> Working with syntactic objects"""
+
+# ╔═╡ 9b390dfb-1685-4cf8-bd37-2137f277a320
+md"""> Parsing a citable corpus into sentences"""
+
 # ╔═╡ d12ab940-f56a-49e0-bedf-b341f2124bc5
 f = joinpath(repo, "test", "data", "hyginus.cex")
 
@@ -40,8 +46,10 @@ typeof(sentences)
 
 # ╔═╡ Cell order:
 # ╟─d3d708ea-5a63-4c9e-9fc2-6801ada726e0
-# ╟─a4471683-d5cd-4ca4-9f3b-c582427f473b
+# ╠═a4471683-d5cd-4ca4-9f3b-c582427f473b
 # ╟─2a2d0160-8794-11ed-23a7-0d6ff795640e
+# ╟─99a2e366-4e46-401b-bae9-241e98425e97
+# ╟─9b390dfb-1685-4cf8-bd37-2137f277a320
 # ╟─d12ab940-f56a-49e0-bedf-b341f2124bc5
 # ╠═059b137a-a5ff-4b9e-935b-b00effdf0a9f
 # ╠═7f9fd244-f16b-47fe-8dd8-12e06152b733
